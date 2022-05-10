c45792a9fcc0b2715a3cb93d8a32d749ec9b00f5
kevin-scholten <kevin.scholten@topicus.nl>
2022-05-10 16:40:54 +0200

    Initial commit

---
+++ src/test/java/com/example/playgroundprojectselenium/Helper.java
@@ -0,0 +1,34 @@
+package com.example.playgroundprojectselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Helper {
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+    public static String remote_url_chrome = "http://localhost:4444/wd/hub";
+    public static String remote_url_firefox = "http://localhost:4444/wd/hub";
+
+    public void setupThread(String browserName) throws MalformedURLException {
+        if(browserName.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            driver.set(new RemoteWebDriver(new URL(remote_url_chrome), options));
+        }
+        else if(browserName.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            driver.set(new RemoteWebDriver(new URL(remote_url_firefox), options));
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public void tearDownDriver() {
+        getDriver().quit();
+    }
+}
